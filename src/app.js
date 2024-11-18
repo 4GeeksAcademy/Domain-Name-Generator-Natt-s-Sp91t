022c6214f833db6e61cebd32d57bf16f2fa03c6a
Natalia Manzano Carnerero <mcn.sdevelop@gmail.com>
2024-11-18 11:16:08 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,23 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+let pronombre = ["la", "el", "nuestra", "tu"];
+let adjetivo = ["mejor", "nueva", "lujosa", "profesional"];
+let nombre = ["ropa", "cafe", "musica", "libros"];
+let dominio = [".com", ".net", ".es", ".org"];
+let hackDomain = ["restaurant.es"];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+for (let pron of pronombre) {
+  for (let adj of adjetivo) {
+    for (let nom of nombre) {
+      for (let dom of dominio) {
+        console.log(pron + adj + nom + dom);
+      }
+    }
+  }
+}
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+for (let pron of pronombre) {
+  for (let adj of adjetivo) {
+    for (let hack of hackDomain) {
+      console.log(pron + adj + hack);
+    }
+  }
+}
